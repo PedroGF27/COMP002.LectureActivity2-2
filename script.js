fe8f0ef45e824db9b965da5f50dcc0564a8ecc4f
Pedro Garcia <coolcatdopedog@gmail.com>
2024-10-23 19:52:48 -0700

    gave explanation to answers

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ console.log(myGreeting);
 myGreeting = "Changed the value of the string variable here."
 console.log(myGreeting)
 
+// const variable
 const pi = 3.14
-// pi = 2 - Shows an error
+// pi = 2 - Shows an error because pi is always 3.14
 
 let myNumber = "27"
 console.log(Math.min(pi, myNumber));
+// 3.14 < 27 myNumber = 27 pi = 3.14 
