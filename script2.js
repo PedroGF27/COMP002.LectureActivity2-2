1b533d9197eecb28d01894f108857a34744ae70f
Pedro Garcia <coolcatdopedog@gmail.com>
2024-10-23 20:46:09 -0700

    added loop to javascript

---
+++ script2.js
@@ -0,0 +1,26 @@
+
+let myGrade = 72
+
+if (myGrade >= 90) {
+    console.log("A")
+}
+else if (myGrade < 89 && myGrade > 80) {
+    console.log("B")
+}
+else if (myGrade < 79 && myGrade > 70) {
+    console.log("C")
+}
+else if (myGrade < 69 && myGrade > 60) {
+    console.log("D")
+}
+else {
+    console.log("F")
+}
+
+for(let i = 0;i <= 100; i++) {
+    if(i%2 == 1){
+        console.log("!")
+    }
+    else (console.log(i))
+}
+
