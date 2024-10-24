4187c2dc2d94a1372e447f86b8698cd280447d5c
Pedro Garcia <coolcatdopedog@gmail.com>
2024-10-23 19:43:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// lecture 2-2
+// string variable
+let myGreeting = "Hello world"
+
+console.log(myGreeting);
+
+// change the string variable value
+myGreeting = "Changed the value of the string variable here."
+console.log(myGreeting)
+
+const pi = 3.14
+// pi = 2 - Shows an error
+
+let myNumber = "27"
+console.log(Math.min(pi, myNumber));
